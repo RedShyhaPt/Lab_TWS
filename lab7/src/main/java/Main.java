2895b3433e55abdab71d23a23483c48fd41aa60d
RedShyhaPt <64642070+RedShyhaPt@users.noreply.github.com>
2022-06-07 17:20:45 +0300

    add lab 7 - publisher & browser

--- lab7/src/main/java/Main.java
+++ lab7/src/main/java/Main.java
@@ -9,28 +9,28 @@ public class Main {
     public static void main(String[] args) {
         // Регистрация сервиса
         System.out.println("Publish new service");
-        //System.out.println("Enter business name: ");
-        String businessName = "ProductShop";
-        //System.out.println("Enter service name: ");
-        String serviceName = "ClientService";
+        String businessName = "Shopper";
+        String serviceName = "ClientServices";
         System.out.println("Business " + businessName + ", service " + serviceName + " will be created");
-
+// Регестрируем в Juddi
+        Publisher publisher = new Publisher();
+        publisher.publish(businessName, serviceName, URL);
+        System.out.println("--------------------------------\n\n");
+        /*
         // Показать все сервисы
         System.out.println("Search service");
         Browser browser = new Browser();
         browser.ViewAll();
         System.out.println("--------------------------------\n\n");
-
-        /*
-        // Регестрируем в Juddi
-        Publisher publisher = new Publisher();
-        publisher.publish(businessName, serviceName, URL);
-        System.out.println("--------------------------------\n\n");
-        // Поиск сервиса по имени
+// Поиск сервиса по имени
         System.out.println("Find service by name");
-        String name = "ClientService";
+        String name = "Client_Service";
         Browser searchService = new Browser();
         searchService.findServiceByName(name);
-        System.out.println("--------------------------------\n\n");*/
+        System.out.println("--------------------------------\n\n");
+
+*/
+
+
     }
 }
